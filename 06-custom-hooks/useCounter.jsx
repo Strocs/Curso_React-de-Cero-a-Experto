c4ef6a2fc4b04dc3a9ff9f2ddcb52d3b19aa6285
Strocs <iganmolina@gmail.com>
2022-12-06 01:21:00 -0300

    add@ custom hooks folder

---
+++ 06-custom-hooks/useCounter.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+
+export const useCounter = (initialValue = 0) => {
+  const [counter, setCounter] = useState(initialValue)
+
+  const increment = (value = 1) => {
+    setCounter(current => current + value)
+  }
+
+  const reset = () => {
+    setCounter(initialValue)
+  }
+
+  const decrement = (value = 1) => {
+    if (counter === 0) return
+    setCounter(current => current - value)
+  }
+  useEffect(() => {
+    setCounter(initialValue)
+  }, [initialValue])
+
+  return {
+    counter,
+    increment,
+    reset,
+    decrement
+  }
+}
