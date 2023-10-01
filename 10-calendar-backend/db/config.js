041980ff46b268bcf6bcc2a3f39b407f4fcacfd3
Ignacio A. Molina <strocsdev@gmail.com>
2023-10-01 19:52:49 -0300

    change mongoose catch promise

--- 10-calendar-backend/db/config.js
+++ 10-calendar-backend/db/config.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose')
 
 const dbConnection = async () => {
-  try {
-    await mongoose.connect(process.env.DB_CNN)
-
-    console.log('DB Online')
-  } catch (error) {
-    console.log(error)
-    throw new Error('Connection to DB failed')
-  }
+  await mongoose
+    .connect(process.env.DB_CNN)
+    .then(() => console.log('DB Online'))
+    .catch(error => {
+      console.log(error)
+      throw new Error('Connection to DB failed')
+    })
 }
 
 module.exports = {
