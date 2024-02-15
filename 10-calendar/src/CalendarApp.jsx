740d862d21c6d647566d9b6273d85a0b6e08c967
Ignacio A. Molina <strocsdev@gmail.com>
2024-02-15 14:14:59 -0300

    fix vercel reload page error

--- 10-calendar/src/CalendarApp.jsx
+++ 10-calendar/src/CalendarApp.jsx
@@ -2,12 +2,15 @@ import { BrowserRouter } from 'react-router-dom'
 import { AppRouter } from './router'
 import { Provider } from 'react-redux'
 import { store } from './store'
+// import { HashRouter } from 'react-router-dom'
 
 export const CalendarApp = () => {
   return (
     <Provider store={store}>
       <BrowserRouter>
+        {/* <HashRouter> */}
         <AppRouter />
+        {/* </HashRouter> */}
       </BrowserRouter>
     </Provider>
   )
